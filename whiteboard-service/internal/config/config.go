7c9556a5fb9f3f4f6d86d0bb28ba459426b034f1
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2025-06-03 17:13:24 +0300

    feat(config): `DatabaseConfig`

--- whiteboard-service/internal/config/config.go
+++ whiteboard-service/internal/config/config.go
@@ -3,28 +3,32 @@ package config
 import (
 	"flag"
 	"os"
-	"time"
 
 	"github.com/ilyakaznacheev/cleanenv"
 )
 
 type Config struct {
-	Env  EnvType    `yaml:"env" env-default:"local"`
-	GRPC GRPCConfig `yaml:"grpc_server"`
+	Env      EnvType        `yaml:"env" env-default:"local"`
+	GRPC     GRPCConfig     `yaml:"grpc_server"`
+	Database DatabaseConfig `yaml:"database"`
 }
 
-type GRPCConfig struct {
-	Port    int           `yaml:"port"`
-	Timeout time.Duration `yaml:"timeout"`
-}
-
-func MustLoad() *Config {
-	configPath := fetchConfigPath()
+func MustLoad(configPath string) *Config {
 	if configPath == "" {
 		panic("config path is empty")
 	}
 
-	return MustLoadPath(configPath)
+		// check if file exists
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		panic("config file does not exist: " + configPath)
+	}
+
+	var cfg Config
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		panic("cannot read config: " + err.Error())
+	}
+
+	return &cfg
 }
 
 func MustLoadPath(configPath string) *Config {
