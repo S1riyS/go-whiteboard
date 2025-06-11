438cf118f4d61855c8cf1aed3b89f2e4644b5036
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2025-06-11 22:56:56 +0300

    fix(config): add `Address` method to `RedisConfig`

--- services/collaboration-service/internal/config/redis_config.go
+++ services/collaboration-service/internal/config/redis_config.go
@@ -1,8 +1,14 @@
 package config
 
+import "fmt"
+
 type RedisConfig struct {
 	Host     string `yaml:"host"`
 	Port     int    `yaml:"port"`
 	Password string `yaml:"password"`
-	Database string `yaml:"database"`
+	Database int    `yaml:"database"`
+}
+
+func (c *RedisConfig) Address() string {
+	return fmt.Sprintf("%s:%d", c.Host, c.Port)
 }
