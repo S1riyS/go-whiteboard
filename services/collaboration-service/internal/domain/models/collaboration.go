577b5fcf6e8e593dfc5c7cb6879bba42459fdb49
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2025-06-19 15:47:31 +0300

    feat(models): add `ElementType` "enum"
    Will be refactored

--- services/collaboration-service/internal/domain/models/collaboration.go
+++ services/collaboration-service/internal/domain/models/collaboration.go
@@ -5,10 +5,16 @@ import (
 	"time"
 )
 
+const (
+	ElementTypeLine   = "line"
+	ElementTypeCircle = "circle"
+	ElementTypeStroke = "stroke"
+)
+
 type Drawing struct {
-	WhiteboardID int             `json:"whiteboard_id"`
+	WhiteboardID int             `json:"whiteboard_id"` // TODO: decide if this should be int, string or something else
 	Timestamp    time.Time       `json:"timestamp"`
-	ClientID     int             `json:"client_id"`
+	ClientID     int             `json:"client_id"` // TODO: decide if this should be int, string or something else
 	ElementType  string          `json:"element_type"`
-	Element      json.RawMessage `json:"element"`
+	Element      json.RawMessage `json:"element"` //ElementStroke | ElementLine | ElementCircle
 }
