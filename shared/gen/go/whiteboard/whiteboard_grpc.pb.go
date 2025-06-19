3c18985cc432909bb57e31144759723c15575e8d
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2025-06-19 21:39:01 +0300

    ♻️ refactor(proto): rename service, is as a string

--- shared/gen/go/whiteboard/whiteboard_grpc.pb.go
+++ shared/gen/go/whiteboard/whiteboard_grpc.pb.go
@@ -19,194 +19,194 @@ import (
 // Requires gRPC-Go v1.32.0 or later.
 const _ = grpc.SupportPackageIsVersion7
 
-// WhiteboardServiceClient is the client API for WhiteboardService service.
+// WhiteboardV1Client is the client API for WhiteboardV1 service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
-type WhiteboardServiceClient interface {
+type WhiteboardV1Client interface {
 	CreateWhiteboard(ctx context.Context, in *CreateWhiteboardRequest, opts ...grpc.CallOption) (*WhiteboardResponse, error)
 	GetWhiteboard(ctx context.Context, in *GetWhiteboardRequest, opts ...grpc.CallOption) (*WhiteboardResponse, error)
 	UpdateWhiteboard(ctx context.Context, in *UpdateWhiteboardRequest, opts ...grpc.CallOption) (*WhiteboardResponse, error)
 	DeleteWhiteboard(ctx context.Context, in *DeleteWhiteboardRequest, opts ...grpc.CallOption) (*empty.Empty, error)
 }
 
-type whiteboardServiceClient struct {
+type whiteboardV1Client struct {
 	cc grpc.ClientConnInterface
 }
 
-func NewWhiteboardServiceClient(cc grpc.ClientConnInterface) WhiteboardServiceClient {
-	return &whiteboardServiceClient{cc}
+func NewWhiteboardV1Client(cc grpc.ClientConnInterface) WhiteboardV1Client {
+	return &whiteboardV1Client{cc}
 }
 
-func (c *whiteboardServiceClient) CreateWhiteboard(ctx context.Context, in *CreateWhiteboardRequest, opts ...grpc.CallOption) (*WhiteboardResponse, error) {
+func (c *whiteboardV1Client) CreateWhiteboard(ctx context.Context, in *CreateWhiteboardRequest, opts ...grpc.CallOption) (*WhiteboardResponse, error) {
 	out := new(WhiteboardResponse)
-	err := c.cc.Invoke(ctx, "/github.com.s1riys.go_whiteboard.WhiteboardService/CreateWhiteboard", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/github.com.s1riys.go_whiteboard.WhiteboardV1/CreateWhiteboard", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *whiteboardServiceClient) GetWhiteboard(ctx context.Context, in *GetWhiteboardRequest, opts ...grpc.CallOption) (*WhiteboardResponse, error) {
+func (c *whiteboardV1Client) GetWhiteboard(ctx context.Context, in *GetWhiteboardRequest, opts ...grpc.CallOption) (*WhiteboardResponse, error) {
 	out := new(WhiteboardResponse)
-	err := c.cc.Invoke(ctx, "/github.com.s1riys.go_whiteboard.WhiteboardService/GetWhiteboard", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/github.com.s1riys.go_whiteboard.WhiteboardV1/GetWhiteboard", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *whiteboardServiceClient) UpdateWhiteboard(ctx context.Context, in *UpdateWhiteboardRequest, opts ...grpc.CallOption) (*WhiteboardResponse, error) {
+func (c *whiteboardV1Client) UpdateWhiteboard(ctx context.Context, in *UpdateWhiteboardRequest, opts ...grpc.CallOption) (*WhiteboardResponse, error) {
 	out := new(WhiteboardResponse)
-	err := c.cc.Invoke(ctx, "/github.com.s1riys.go_whiteboard.WhiteboardService/UpdateWhiteboard", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/github.com.s1riys.go_whiteboard.WhiteboardV1/UpdateWhiteboard", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *whiteboardServiceClient) DeleteWhiteboard(ctx context.Context, in *DeleteWhiteboardRequest, opts ...grpc.CallOption) (*empty.Empty, error) {
+func (c *whiteboardV1Client) DeleteWhiteboard(ctx context.Context, in *DeleteWhiteboardRequest, opts ...grpc.CallOption) (*empty.Empty, error) {
 	out := new(empty.Empty)
-	err := c.cc.Invoke(ctx, "/github.com.s1riys.go_whiteboard.WhiteboardService/DeleteWhiteboard", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/github.com.s1riys.go_whiteboard.WhiteboardV1/DeleteWhiteboard", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// WhiteboardServiceServer is the server API for WhiteboardService service.
-// All implementations must embed UnimplementedWhiteboardServiceServer
+// WhiteboardV1Server is the server API for WhiteboardV1 service.
+// All implementations must embed UnimplementedWhiteboardV1Server
 // for forward compatibility
-type WhiteboardServiceServer interface {
+type WhiteboardV1Server interface {
 	CreateWhiteboard(context.Context, *CreateWhiteboardRequest) (*WhiteboardResponse, error)
 	GetWhiteboard(context.Context, *GetWhiteboardRequest) (*WhiteboardResponse, error)
 	UpdateWhiteboard(context.Context, *UpdateWhiteboardRequest) (*WhiteboardResponse, error)
 	DeleteWhiteboard(context.Context, *DeleteWhiteboardRequest) (*empty.Empty, error)
-	mustEmbedUnimplementedWhiteboardServiceServer()
+	mustEmbedUnimplementedWhiteboardV1Server()
 }
 
-// UnimplementedWhiteboardServiceServer must be embedded to have forward compatible implementations.
-type UnimplementedWhiteboardServiceServer struct {
+// UnimplementedWhiteboardV1Server must be embedded to have forward compatible implementations.
+type UnimplementedWhiteboardV1Server struct {
 }
 
-func (UnimplementedWhiteboardServiceServer) CreateWhiteboard(context.Context, *CreateWhiteboardRequest) (*WhiteboardResponse, error) {
+func (UnimplementedWhiteboardV1Server) CreateWhiteboard(context.Context, *CreateWhiteboardRequest) (*WhiteboardResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method CreateWhiteboard not implemented")
 }
-func (UnimplementedWhiteboardServiceServer) GetWhiteboard(context.Context, *GetWhiteboardRequest) (*WhiteboardResponse, error) {
+func (UnimplementedWhiteboardV1Server) GetWhiteboard(context.Context, *GetWhiteboardRequest) (*WhiteboardResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetWhiteboard not implemented")
 }
-func (UnimplementedWhiteboardServiceServer) UpdateWhiteboard(context.Context, *UpdateWhiteboardRequest) (*WhiteboardResponse, error) {
+func (UnimplementedWhiteboardV1Server) UpdateWhiteboard(context.Context, *UpdateWhiteboardRequest) (*WhiteboardResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method UpdateWhiteboard not implemented")
 }
-func (UnimplementedWhiteboardServiceServer) DeleteWhiteboard(context.Context, *DeleteWhiteboardRequest) (*empty.Empty, error) {
+func (UnimplementedWhiteboardV1Server) DeleteWhiteboard(context.Context, *DeleteWhiteboardRequest) (*empty.Empty, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method DeleteWhiteboard not implemented")
 }
-func (UnimplementedWhiteboardServiceServer) mustEmbedUnimplementedWhiteboardServiceServer() {}
+func (UnimplementedWhiteboardV1Server) mustEmbedUnimplementedWhiteboardV1Server() {}
 
-// UnsafeWhiteboardServiceServer may be embedded to opt out of forward compatibility for this service.
-// Use of this interface is not recommended, as added methods to WhiteboardServiceServer will
+// UnsafeWhiteboardV1Server may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to WhiteboardV1Server will
 // result in compilation errors.
-type UnsafeWhiteboardServiceServer interface {
-	mustEmbedUnimplementedWhiteboardServiceServer()
+type UnsafeWhiteboardV1Server interface {
+	mustEmbedUnimplementedWhiteboardV1Server()
 }
 
-func RegisterWhiteboardServiceServer(s grpc.ServiceRegistrar, srv WhiteboardServiceServer) {
-	s.RegisterService(&WhiteboardService_ServiceDesc, srv)
+func RegisterWhiteboardV1Server(s grpc.ServiceRegistrar, srv WhiteboardV1Server) {
+	s.RegisterService(&WhiteboardV1_ServiceDesc, srv)
 }
 
-func _WhiteboardService_CreateWhiteboard_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _WhiteboardV1_CreateWhiteboard_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(CreateWhiteboardRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(WhiteboardServiceServer).CreateWhiteboard(ctx, in)
+		return srv.(WhiteboardV1Server).CreateWhiteboard(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/github.com.s1riys.go_whiteboard.WhiteboardService/CreateWhiteboard",
+		FullMethod: "/github.com.s1riys.go_whiteboard.WhiteboardV1/CreateWhiteboard",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(WhiteboardServiceServer).CreateWhiteboard(ctx, req.(*CreateWhiteboardRequest))
+		return srv.(WhiteboardV1Server).CreateWhiteboard(ctx, req.(*CreateWhiteboardRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _WhiteboardService_GetWhiteboard_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _WhiteboardV1_GetWhiteboard_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(GetWhiteboardRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(WhiteboardServiceServer).GetWhiteboard(ctx, in)
+		return srv.(WhiteboardV1Server).GetWhiteboard(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/github.com.s1riys.go_whiteboard.WhiteboardService/GetWhiteboard",
+		FullMethod: "/github.com.s1riys.go_whiteboard.WhiteboardV1/GetWhiteboard",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(WhiteboardServiceServer).GetWhiteboard(ctx, req.(*GetWhiteboardRequest))
+		return srv.(WhiteboardV1Server).GetWhiteboard(ctx, req.(*GetWhiteboardRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _WhiteboardService_UpdateWhiteboard_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _WhiteboardV1_UpdateWhiteboard_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(UpdateWhiteboardRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(WhiteboardServiceServer).UpdateWhiteboard(ctx, in)
+		return srv.(WhiteboardV1Server).UpdateWhiteboard(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/github.com.s1riys.go_whiteboard.WhiteboardService/UpdateWhiteboard",
+		FullMethod: "/github.com.s1riys.go_whiteboard.WhiteboardV1/UpdateWhiteboard",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(WhiteboardServiceServer).UpdateWhiteboard(ctx, req.(*UpdateWhiteboardRequest))
+		return srv.(WhiteboardV1Server).UpdateWhiteboard(ctx, req.(*UpdateWhiteboardRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _WhiteboardService_DeleteWhiteboard_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _WhiteboardV1_DeleteWhiteboard_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(DeleteWhiteboardRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(WhiteboardServiceServer).DeleteWhiteboard(ctx, in)
+		return srv.(WhiteboardV1Server).DeleteWhiteboard(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/github.com.s1riys.go_whiteboard.WhiteboardService/DeleteWhiteboard",
+		FullMethod: "/github.com.s1riys.go_whiteboard.WhiteboardV1/DeleteWhiteboard",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(WhiteboardServiceServer).DeleteWhiteboard(ctx, req.(*DeleteWhiteboardRequest))
+		return srv.(WhiteboardV1Server).DeleteWhiteboard(ctx, req.(*DeleteWhiteboardRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-// WhiteboardService_ServiceDesc is the grpc.ServiceDesc for WhiteboardService service.
+// WhiteboardV1_ServiceDesc is the grpc.ServiceDesc for WhiteboardV1 service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
-var WhiteboardService_ServiceDesc = grpc.ServiceDesc{
-	ServiceName: "github.com.s1riys.go_whiteboard.WhiteboardService",
-	HandlerType: (*WhiteboardServiceServer)(nil),
+var WhiteboardV1_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "github.com.s1riys.go_whiteboard.WhiteboardV1",
+	HandlerType: (*WhiteboardV1Server)(nil),
 	Methods: []grpc.MethodDesc{
 		{
 			MethodName: "CreateWhiteboard",
-			Handler:    _WhiteboardService_CreateWhiteboard_Handler,
+			Handler:    _WhiteboardV1_CreateWhiteboard_Handler,
 		},
 		{
 			MethodName: "GetWhiteboard",
-			Handler:    _WhiteboardService_GetWhiteboard_Handler,
+			Handler:    _WhiteboardV1_GetWhiteboard_Handler,
 		},
 		{
 			MethodName: "UpdateWhiteboard",
-			Handler:    _WhiteboardService_UpdateWhiteboard_Handler,
+			Handler:    _WhiteboardV1_UpdateWhiteboard_Handler,
 		},
 		{
 			MethodName: "DeleteWhiteboard",
-			Handler:    _WhiteboardService_DeleteWhiteboard_Handler,
+			Handler:    _WhiteboardV1_DeleteWhiteboard_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
